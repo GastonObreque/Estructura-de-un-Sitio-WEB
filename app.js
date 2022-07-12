0b956ccf7dc66f0f9fb5c956dfa998849899df1a
GastonObreque <gambexequiel@hotmail.com>
2022-07-11 21:37:21 -0300

    agrego imagenes a views

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const path = require('path')
 const app = express();
 const port = 3030;
 
-app.listen(port, () => {
-    console.log('Server runnin in http://localhost:' + port);
-})
+app.use(express.static('public'));
+
+app.get('/', (req,res) => res.sendFile(path.join(__dirname,'views', 'home.html')));
+app.get('/bunner', (req,res) => res.sendFile(path.join(__dirname,'views', 'bunner.html')));
+app.get('/cafetera', (req,res) => res.sendFile(path.join(__dirname,'views', 'cafetera.html')));
+app.get('/macbook', (req,res) => res.sendFile(path.join(__dirname,'views', 'macbook.html')));
+app.get('/samsung', (req,res) => res.sendFile(path.join(__dirname,'views', 'samsung.html')));
+app.get('/smart', (req,res) => res.sendFile(path.join(__dirname,'views', 'smart.html')));
+
+app.listen(port, () =>
+    console.log('Server running in http://localhost:' + port));
